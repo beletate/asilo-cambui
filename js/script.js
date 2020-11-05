d6d8011efe75a66c0b532f96f43025a74d189e2c
Vinicius Beletate <vinicius_beletate@hotmail.com>
2020-11-05 14:27:51 -0300

    Adicionando o text area

--- js/script.js
+++ js/script.js
@@ -1,24 +1,37 @@
-start()
 
-function start(){
+function enviar() {
 
-    fetch('https://news-api-node.herokuapp.com/api/v1/news/ea30897c-67fe-4f79-896a-369366a914bb')
-    .then(results => results.json())
-    .then(console.log)
-    
+    var comment = document.getElementById("texto").value
 
-    var poster = { 
-        post: 'GET'
-     };
+    console.log(comment)
 
-    fetch('https://news-api-node.herokuapp.com/api/v1/news/ea30897c-67fe-4f79-896a-369366a914bb', 
-    { 
-        method: 'post',
-        headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'}, 
-        body: JSON.stringify(poster)})
+
+    var poster = {
+        post: `${comment}`
+    };
+
+
+    fetch('https://news-api-node.herokuapp.com/api/v1/news/ea30897c-67fe-4f79-896a-369366a914bb',
+        {
+            method: 'post',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(poster)
+        })
 
         .then(results => results.json())
         .then(console.log)
-    }
+
+    fetch('https://news-api-node.herokuapp.com/api/v1/news/ea30897c-67fe-4f79-896a-369366a914bb/')
+        .then(function (response) {
+            response.json()
+            
+            .then(function (result) {
+                document.getElementById("valores").innerHTML = JSON.stringify(result[1])
+                    console.log(result);
+                })
+        })
+
+}
