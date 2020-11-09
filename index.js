5d64b1e733689e5ed7e63dbf0cad804ebc1454a3
Vinicius Beletate <vinicius_beletate@hotmail.com>
2020-11-09 16:34:13 -0300

    refactor: new classes all the header

--- index.js
+++ index.js
@@ -2,21 +2,27 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
 
 (async function example() {
   let driver = await new Builder().forBrowser('chrome').build();
-  try {
 
     /* await driver.get('http://www.google.com/ncr');
     await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
     
-    await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
     
     */
    await driver.get('https://beletate.github.io/asilo-cambui/index.html');
+   await driver.findElement(By.className('doacaoStation')).click();
+   await driver.wait(until.titleIs('Asilo Cambuí'), 5000);
+   await driver.findElement(By.className('conta-bancaria')).click();
+   await driver.wait(until.titleIs('Asilo Cambuí'), 5000);
+   await driver.findElement(By.className('contatoStation')).click();
+   await driver.wait(until.titleIs('Asilo Cambuí'), 5000);
    await driver.findElement(By.className('bazarStation')).click();
-    
-    
-  } finally {
-    await driver.quit();
-  }
+   await driver.findElement(By.className('btn success')).click();
+   await driver.wait(until.titleIs('Asilo Cambuí'), 5000);
+   await driver.findElement(By.className('eventosStation')).click();
+   await driver.wait(until.titleIs('Asilo Cambuí'), 5000);
+   driver.execute_script("window.scrollTo(0, 600)") 
+   driver.execute_script("window.scrollTo(0, 0)") 
+   
 })();
 
 
