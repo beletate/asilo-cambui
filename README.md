fe97b4fb0a715a44a9ce66fafda6b09192d64745
Vinicius Beletate <vinicius_beletate@hotmail.com>
2020-11-09 17:25:05 -0300

    Terceira parte trabalho

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Confira: [Asilo Cambuí](https://beletate.github.io/asilo-cambui/)
+
+## Instruções:
+
+1. `npm i` para realizar o download de todas as dependências.
+2. `node index` para executar o teste automatizado.
--- index.js
+++ index.js
@@ -3,54 +3,25 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
 (async function example() {
   let driver = await new Builder().forBrowser('chrome').build();
 
-    /* await driver.get('http://www.google.com/ncr');
-    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-    
-    
-    */
    await driver.get('https://beletate.github.io/asilo-cambui/index.html');
    await driver.findElement(By.className('doacaoStation')).click();
-   await driver.wait(until.titleIs('Asilo Cambuí'), 5000);
+   await driver.wait(until.titleIs('Asilo Cambuí'), 100000);
    await driver.findElement(By.className('conta-bancaria')).click();
-   await driver.wait(until.titleIs('Asilo Cambuí'), 5000);
+   await driver.wait(until.titleIs('Asilo Cambuí'), 100000);
    await driver.findElement(By.className('contatoStation')).click();
-   await driver.wait(until.titleIs('Asilo Cambuí'), 5000);
+   await driver.wait(until.titleIs('Asilo Cambuí'), 100000);
    await driver.findElement(By.className('bazarStation')).click();
-   await driver.findElement(By.className('btn success')).click();
-   await driver.wait(until.titleIs('Asilo Cambuí'), 5000);
+   await driver.wait(until.titleIs('Asilo Cambuí'), 100000);
    await driver.findElement(By.className('eventosStation')).click();
-   await driver.wait(until.titleIs('Asilo Cambuí'), 5000);
-   driver.execute_script("window.scrollTo(0, 600)") 
-   driver.execute_script("window.scrollTo(0, 0)") 
+   await driver.wait(until.titleIs('Asilo Cambuí'), 100000);
+   await driver.findElement(By.className('homeStation')).click();
+   await driver.wait(until.titleIs('Asilo Cambuí'), 100000);
+   await driver.findElement(By.className('postStation')).click();
+   await driver.wait(until.titleIs('Asilo Cambuí'), 100000);
+   await driver.findElement(By.className('doacaoStation')).click();
+   await driver.wait(until.titleIs('Asilo Cambuí'), 100000);
+   await driver.findElement(By.className('contatoStation')).click();
+   await driver.wait(until.titleIs('Asilo Cambuí'), 100000);
+   await driver.quit();
    
 })();
-
-
-/* 
-await driver.findElement(By.name('a')).sendKeys('webdriver', Key.RETURN);
-
-
-await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-
-
-
-var webdriver = require('selenium-webdriver');
-var chrome = require('selenium-webdriver/chrome');
-var path = require('chromedriver').path;
-
-var service = new chrome.ServiceBuilder(path).build();
-chrome.setDefaultService(service);
-
-var driver = new webdriver.Builder()
-.withCapabilities(webdriver.Capabilities.chrome())
-.build();
-
-(async function example() {
-  try {
-    await driver.get('http://www.google.com/ncr');
-    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-    await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-  } finally {
-    await driver.quit();
-  }
-})(); */
