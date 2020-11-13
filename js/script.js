427f4e446523367bad6965539a2efe5cbedafc0a
Vinicius Beletate <vinicius_beletate@hotmail.com>
2020-11-13 20:05:04 -0300

    Adicionando aba comentarios e puxando api

--- js/script.js
+++ js/script.js
@@ -1,17 +1,24 @@
 
+const urlApi = 'https://news-api-node.herokuapp.com/api/v1/news/ea30897c-67fe-4f79-896a-369366a914bb'
+const postFeitos = document.querySelector('.post-feitos')
+
+const novosPosts = (novaMensagem, novoElemento) => {
+    const elemento = novoElemento
+    const mensagem = novaMensagem
+    elemento.appendChild(mensagem)
+    postFeitos.appendChild(elemento)
+}
+
 function enviar() {
 
     var comment = document.getElementById("texto").value
-
-    console.log(comment)
-
 
     var poster = {
         post: `${comment}`
     };
 
 
-    fetch('https://news-api-node.herokuapp.com/api/v1/news/ea30897c-67fe-4f79-896a-369366a914bb',
+    fetch(urlApi,
         {
             method: 'post',
             headers: {
@@ -23,15 +30,25 @@ function enviar() {
 
         .then(results => results.json())
         .then(console.log)
-
-    fetch('https://news-api-node.herokuapp.com/api/v1/news/ea30897c-67fe-4f79-896a-369366a914bb/')
-        .then(function (response) {
-            response.json()
-            
-            .then(function (result) {
-                document.getElementById("valores").innerHTML = JSON.stringify(result[1])
-                    console.log(result);
-                })
-        })
+    start()
 
 }
+
+const start = () => {
+    fetch(urlApi)
+        .then(function (response) {
+            response.json()
+
+                .then(function (data) {
+
+
+                    data.forEach(dados => {
+                        var novaMensagem = document.createTextNode(dados.post)
+                        var novoElemento = document.createElement('p')
+                        novosPosts(novaMensagem, novoElemento)
+                    })
+
+                })
+        })
+}
+start()
